d265ed54963aaada2dc722debc02f167196f5f10
fangpc <fangpc@asiainfo.com>
2020-09-12 15:35:57 +0800

    concurrent demo start

---
+++ src/main/java/com/pc/concurrent/ConcurrentDemoApplication.java
@@ -0,0 +1,16 @@
+package com.pc.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fangpengcheng
+ */
+@SpringBootApplication
+public class ConcurrentDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentDemoApplication.class, args);
+    }
+
+}
